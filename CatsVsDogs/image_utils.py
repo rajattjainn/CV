f6aa40bd55ab3b3b41a3e8df98278d03b8838492
Rajat Jain <tojainrajat@gmail.com>
2022-01-24 15:39:09 +0530

    Read all the input files in Data folder and return a dataset
    Based on the name of input files, the Dataset will have lables as
    either 1 (for Dog) or 0 (for Cat)

---
+++ CatsVsDogs/image_utils.py
@@ -0,0 +1,43 @@
+import os
+
+from torch.utils.data import Dataset
+import torch
+from PIL import Image
+import torchvision.transforms as transforms 
+
+class DogsCats(Dataset):
+    def __init__(self, folder_path) -> None:
+        super().__init__()
+        self.folder_path = folder_path
+        self.pixel_data, self.label_data = self.read_input_data(folder_path)
+
+    def read_input_data(self, folder_path):
+        pixel_data = []
+        label_data = []
+
+        for filename in os.listdir(folder_path):
+
+            if filename.lower().startswith("dog") and filename.endswith("jpg"):
+                label = 1
+            elif filename.lower().startswith("cat") and filename.endswith("jpg"):
+                label = 0
+            else:
+                label = -1
+            
+            if (label == 1 or label == 0):
+                image = Image.open(os.path.join(folder_path, filename))
+                transform = transforms.ToTensor()
+                x_tensor = transform(image)
+
+                pixel_data.append(x_tensor)
+                label_data.append(torch.tensor(label))
+
+
+        return pixel_data, label_data     
+        
+    def __getitem__(self, idx):
+        return self.pixel_data[idx], self.label_data[idx]
+
+    def __len__(self):
+        assert len(self.pixel_data) == len(self.label_data)
+        return len(self.pixel_data)
