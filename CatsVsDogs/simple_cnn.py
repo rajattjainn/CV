fae55637ea61f67cc9cce130039feb15bd90cc21
Rajat Jain <tojainrajat@gmail.com>
2022-01-31 14:31:10 +0530

    edit the cnn to account for input dimensions
    each input has been scaled to 760x1050. In further implementations,
    we'll look at CNNs that are input dimension agnostic.

--- CatsVsDogs/simple_cnn.py
+++ CatsVsDogs/simple_cnn.py
@@ -6,18 +6,23 @@ class SimpleCNN(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 5, stride = 1, padding = 'same'),
+            nn.Conv2d(in_channels = 3, out_channels = 6, kernel_size = 5, stride = 2),
             nn.MaxPool2d(kernel_size = 2),
-            nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5, stride = 1, padding = 'same'),
+            nn.Conv2d(in_channels = 6, out_channels = 16, kernel_size = 5, stride = 2),
             nn.MaxPool2d(kernel_size = 2),
-            nn.Conv2d(in_channels = 16, out_channels = 120, kernel_size = 5, stride = 1, padding = 'same'))
+            nn.Conv2d(in_channels = 16, out_channels = 120, kernel_size = 5, stride = 2),
+            nn.MaxPool2d(kernel_size = 2),
+            nn.Conv2d(in_channels = 120, out_channels = 180, kernel_size = 5, stride = 2))
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features = 7680, out_features = 120),
+            nn.Linear(in_features = 4320, out_features = 1200),
             nn.ReLU(),
-            nn.Linear(in_features = 120, out_features = 84),
+            nn.Linear(in_features = 1200, out_features = 600),
+            nn.ReLU(),
+            nn.Linear(in_features = 600, out_features = 84),
             nn.ReLU(),
             nn.Linear(in_features = 84, out_features = 3)
+
         )
 
     def forward(self,x):
