74f459e76fafb2af1ad0751232956db10ef69e85
Rajat Jain <tojainrajat@gmail.com>
2022-01-31 14:47:44 +0530

    modularize the file - create functions

--- CatsVsDogs/main.py
+++ CatsVsDogs/main.py
@@ -12,8 +12,21 @@ from torchinfo import summary
 from image_utils import DogsCats
 from simple_cnn import SimpleCNN
 
+import logging 
+
+
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 train_data_path = os.path.join(curr_dir, "Data")
+
+logging.basicConfig(filename="std.log", 
+					format='%(asctime)s %(message)s', 
+					filemode='w') 
+
+#Let us Create an object 
+logger=logging.getLogger() 
+
+#Now we are going to Set the threshold of logger to DEBUG 
+logger.setLevel(logging.DEBUG) 
 
 """
 The resize dimensions 768x1050 has been arrived at by iterating
@@ -28,22 +41,31 @@ input_transform = transforms.Compose([
 train_data = DogsCats(train_data_path, input_transform)
 train_loader = DataLoader(train_data, shuffle=True, batch_size=64)
 
-simple_cnn = SimpleCNN()
-batch_size = 64
 
-learning_rate = 1e-3
-loss_fn = nn.CrossEntropyLoss()
-sgd_optimiser = torch.optim.SGD(simple_cnn.parameters(), 
-                lr=learning_rate, momentum = 0.9)
+def train_model(neural_net, train_loader, lr, momentum, 
+            optimiser_type, loss_fn):
+    optimiser = optimiser_type(neural_net.parameters(), 
+                lr=lr, momentum = momentum)
+    for batch, (pixels, labels) in enumerate(train_loader):
+        summary(neural_net, pixels.size())
+        
+        y_preds = neural_net(pixels)
+        loss = loss_fn(y_preds, labels)
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+        print (f"batch: {batch}, loss = {loss.item()}")
+        logger.debug("batch: " + str(batch) + ", loss = " + str(loss.item()))
 
-for batch, (pixels, labels) in enumerate(train_loader):
-    summary(simple_cnn, pixels.size())
-    
-    y_preds = simple_cnn(pixels)
-    loss = loss_fn(y_preds, labels)
-    sgd_optimiser.zero_grad()
-    loss.backward()
-    sgd_optimiser.step()
-    print (f"batch: {batch}, loss = {loss.item()}")
 
-# def train_model():
+def train_simple_cnn():
+    simple_cnn = SimpleCNN()
+    learning_rate = 1e-3
+    loss_fn = nn.CrossEntropyLoss()
+    sgd_optimiser = torch.optim.SGD
+    momentum = 0.9
+    train_model(simple_cnn, train_loader, learning_rate, momentum, 
+            sgd_optimiser, loss_fn)
+
+
+train_simple_cnn()
