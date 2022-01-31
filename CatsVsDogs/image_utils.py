b33516b0e6b034fbbda33bee63a7e30b0c1c7e03
Rajat Jain <tojainrajat@gmail.com>
2022-01-31 14:01:21 +0530

    move the transform over input to init method

--- CatsVsDogs/image_utils.py
+++ CatsVsDogs/image_utils.py
@@ -6,9 +6,10 @@ from PIL import Image
 import torchvision.transforms as transforms 
 
 class DogsCats(Dataset):
-    def __init__(self, folder_path) -> None:
+    def __init__(self, folder_path, input_transform) -> None:
         super().__init__()
         self.folder_path = folder_path
+        self.transform = input_transform
         self.pixel_data, self.label_data = self.read_input_data(folder_path)
 
     def read_input_data(self, folder_path):
@@ -26,8 +27,7 @@ class DogsCats(Dataset):
             
             if (label == 1 or label == 0):
                 image = Image.open(os.path.join(folder_path, filename))
-                transform = transforms.ToTensor()
-                x_tensor = transform(image)
+                x_tensor = self.transform(image)
 
                 pixel_data.append(x_tensor)
                 label_data.append(torch.tensor(label))
