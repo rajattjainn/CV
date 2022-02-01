c7d1e3f0b7e8bc5ee42c09358f9f53068aed1c86
Rajat Jain <tojainrajat@gmail.com>
2022-02-01 11:48:00 +0530

    Add functionality to plot loss function with total iterations

--- CatsVsDogs/main.py
+++ CatsVsDogs/main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,20 +43,45 @@ train_data = DogsCats(train_data_path, input_transform)
 train_loader = DataLoader(train_data, shuffle=True, batch_size=64)
 
 
+#ToDo: put the text outside the text, at bottom or somewhere.
+def plot_loss(x_data, y_data, xlabel, ylabel, title, plot_text):
+    plt.plot(x_data,y_data)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    text = plot_text.split(";")
+    print ("\n\n")
+    print (plot_text)
+    print (text)
+    print ("\n\n")
+    for count in range(len(text)):
+        plt.gcf().text(0.02, 0.2 + count * 0.1, text[count], fontsize=8)
+    plt.subplots_adjust(left=0.2)
+    plt.savefig(title + '_{date:%Y-%m-%d_%H:%M:%S}.png'.format( date=datetime.datetime.now()))
+    
+
 def train_model(neural_net, train_loader, lr, momentum, 
-            optimiser_type, loss_fn):
+            optimiser_type, loss_fn, epochs = 10):
     optimiser = optimiser_type(neural_net.parameters(), 
                 lr=lr, momentum = momentum)
-    for batch, (pixels, labels) in enumerate(train_loader):
-        summary(neural_net, pixels.size())
-        
-        y_preds = neural_net(pixels)
-        loss = loss_fn(y_preds, labels)
-        optimiser.zero_grad()
-        loss.backward()
-        optimiser.step()
-        print (f"batch: {batch}, loss = {loss.item()}")
-        logger.debug("batch: " + str(batch) + ", loss = " + str(loss.item()))
+    y_data = []
+    x_data = []
+    for epoch in range(epochs):
+        for batch, (pixels, labels) in enumerate(train_loader):
+            # summary(neural_net, pixels.size())
+            y_preds = neural_net(pixels)
+            loss = loss_fn(y_preds, labels)
+            y_data.append(loss.item())
+            optimiser.zero_grad()
+            loss.backward()
+            optimiser.step()
+            print (f"batch: {batch}, loss = {loss.item()}")
+            logger.debug("batch: " + str(batch) + ", loss = " + str(loss.item()))
+            x_data.append((epoch + 1) + (batch + 1))
+
+    plot_text = "LR: " + str(lr) + ";momentum: " + str(momentum) + ";epochs: " + str(epochs) + ";optimiser: " + type(optimiser).__name__ + ";Loss fn: " + type(loss_fn).__name__
+    plot_loss(x_data, y_data, xlabel="Total Runs", ylabel="Cost", title = type(neural_net).__name__, plot_text = plot_text)
+
 
 
 def train_simple_cnn():
@@ -64,8 +90,9 @@ def train_simple_cnn():
     loss_fn = nn.CrossEntropyLoss()
     sgd_optimiser = torch.optim.SGD
     momentum = 0.9
+    total_epochs = 10
     train_model(simple_cnn, train_loader, learning_rate, momentum, 
-            sgd_optimiser, loss_fn)
+            sgd_optimiser, loss_fn, total_epochs)
 
 
 train_simple_cnn()
