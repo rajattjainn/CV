9aa7c6f86cf8c0f4d4ae068779fa8d815e3acf93
Rajat Jain <tojainrajat@gmail.com>
2022-02-04 17:01:54 +0530

    read the data during getitem instead of init

--- CatsVsDogs/image_utils.py
+++ CatsVsDogs/image_utils.py
@@ -5,42 +5,41 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms 
 
+#ToDO: Handle cases when file does not start with .jpg 
 class DogsCats(Dataset):
     def __init__(self, folder_path, input_transform) -> None:
         super().__init__()
         self.folder_path = folder_path
         self.transform = input_transform
-        self.pixel_data, self.label_data = self.read_input_data(folder_path)
 
-    def read_input_data(self, folder_path):
-        pixel_data = []
-        label_data = []
 
-        for filename in os.listdir(folder_path):
+    def read_input_data(self, folder_path, idx):
 
-            if filename.lower().startswith("dog") and filename.endswith("jpg"):
-                label = 1
-            elif filename.lower().startswith("cat") and filename.endswith("jpg"):
-                label = 0
-            else:
-                label = -1
+        filename = os.listdir(folder_path)[idx]
+
+        if filename.lower().startswith("dog") and filename.endswith("jpg"):
+            label = 1
+        elif filename.lower().startswith("cat") and filename.endswith("jpg"):
+            label = 0
+        else:
+            label = -1
             
-            if (label == 1 or label == 0):
-                image = Image.open(os.path.join(folder_path, filename))
-                x_tensor = self.transform(image)
+        if (label == 1 or label == 0):
+            image = Image.open(os.path.join(folder_path, filename))
+            x_tensor = self.transform(image)
 
-                pixel_data.append(x_tensor)
-                label_data.append(torch.tensor(label))
+        if label == -1:
+            print (filename)
+            print (folder_path)
+            return
 
-
-        return pixel_data, label_data     
+        return x_tensor, torch.tensor(label)
         
     def __getitem__(self, idx):
-        return self.pixel_data[idx], self.label_data[idx]
+        return self.read_input_data(self.folder_path, idx)
 
     def __len__(self):
-        assert len(self.pixel_data) == len(self.label_data)
-        return len(self.pixel_data)
+        return len(os.listdir(self.folder_path))
 
 
 class DogsCatsPredict(Dataset):
@@ -48,26 +47,21 @@ class DogsCatsPredict(Dataset):
         super().__init__()
         self.folder_path = folder_path
         self.transform = input_transform
-        self.pixel_data, self.filenames = self.read_input_data(folder_path)
 
-    def read_input_data(self, folder_path):
-        pixel_data = []
-        filenames = []
+    def read_input_data(self, folder_path, idx):
 
-        for filename in os.listdir(folder_path):
-
-            if filename.endswith("jpg"):
-                image = Image.open(os.path.join(folder_path, filename))
-                x_tensor = self.transform(image)
-
-                pixel_data.append(x_tensor)
-                filenames.append(os.path.splitext(filename)[0])
-
-        return pixel_data, filenames
-        
+        filename = os.listdir(folder_path)[idx]
+        if filename.endswith("jpg"):
+            image = Image.open(os.path.join(folder_path, filename))
+            x_tensor = self.transform(image)
+        else:
+            print (folder_path)
+        return x_tensor, filename
+    
+       
     def __getitem__(self, idx):
-        return self.pixel_data[idx], self.filenames[idx]
+        return self.read_input_data(self.folder_path, idx)
 
     def __len__(self):
-        assert len(self.pixel_data) == len(self.filenames)
-        return len(self.pixel_data)
+        return len(os.listdir(self.folder_path))
+    
