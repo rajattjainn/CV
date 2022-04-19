d312b56c162ba9f14eddf12ed14bbbe100a12c97
Rajat Jain <addresseerajat@gmail.com>
2022-04-19 16:31:57 +0530

    [WIP]: Delete old detect function

--- yolo/Yolo v3/detect.py
+++ yolo/Yolo v3/detect.py
@@ -7,7 +7,6 @@ import neural_net
 import utils
 
 
-
 def detect(image_dir_path):
     detect_loader = utils.get_dataloader(image_dir_path, None, shuffle=False)
     net = neural_net.Yolo3("assets/config.cfg")
@@ -38,30 +37,3 @@ def detect(image_dir_path):
 
 image_dir_path = "/Users/Jain/code/learnML/yolo/Yolo v3/images"
 detect(image_dir_path)
-
-
-# def detect_old(folder, image):
-#     img = os.path.join(folder, image)
-#     img_tensor = utils.image_to_tensor(img)
-
-#     net = neural_net.Yolo3("assets/config.cfg")
-#     net.load_weights("assets/yolov3.weights")
-#     net.eval()
-#     with torch.no_grad():
-#         print ("\ndetections")
-#         detections = net(img_tensor)
-#         print (len(detections))
-#         print (detections[0].size())
-        
-#         detections = neural_net.analyze_transactions(detections, cnf_thres = 0.5, iou_thres = 0.4)
-#         classes = neural_net.read_classes("assets/coco.names")
-#         neural_net.draw_rectangle(img, detections, classes)
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--folder", default = "images", type=str)
-# parser.add_argument("--name", default = "eagle.jpg", type=str)
-# args = parser.parse_args()
-# image_folder = args.folder
-# image = args.name
-
-# detect_old(image_folder, image)
