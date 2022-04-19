f5a9c2611d35ee3b95d75c67755aaccf9123b252
Rajat Jain <addresseerajat@gmail.com>
2022-04-19 11:10:55 +0530

    [WIP] create a dataset class to provide image and corresponding label

---
+++ yolo/Yolo v3/datasets.py
@@ -0,0 +1,37 @@
+import torch
+from torchvision.io import read_image
+from torch.utils.data import Dataset
+import os
+
+class ObjectDataSet(Dataset):
+    def __init__(self, image_folder_path, label_folder_path = None, transform = None) -> None:
+        super().__init__()
+        self.image_folder = image_folder_path
+        self.label_folder = label_folder_path
+        self.transform = transform
+        
+        self.image_objects =  [f for f in os.listdir(self.image_folder) if f.endswith(('.jpg', '.jpeg', 'png'))]
+        # if self.label_folder is not None:
+        #     self.label_objects = [f for f in os.listdir(self.label_folder) if f.endswith(('.txt'))]
+            
+    
+    def __getitem__(self, idx):
+        image_path = os.path.join(self.image_folder, self.image_objects[idx])
+        image = read_image(image_path)
+        if self.transform:
+            image = self.transform(image)
+
+        if self.label_folder:
+            label_path = os.path.join(self.label_folder, self.image_objects[idx].split(".")[0] + ".txt")
+            with open(label_path) as f:
+                lines = f.read().splitlines()
+            return image, lines
+        else:
+            return image
+
+    def __len__(self):
+        if self.label_folder is not None:
+            assert len(self.label_objects) == len(self.image_objects)
+        
+        return len(self.image_objects)
+        
