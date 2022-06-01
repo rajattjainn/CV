acb33c887fad6f7878f69d639fdb6aba1dab0da6
Rajat Jain <addresseerajat@gmail.com>
2022-06-01 09:24:13 +0530

    [WIP]: save networks and iteration_loss plot

--- CV/GANs/self_dcgan.py
+++ CV/GANs/self_dcgan.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
 
 import discriminator as dsc
 import generator as gnr
@@ -14,7 +16,7 @@ latent_vector_size = 100
 lr = 0.001
 beta1 = 0.9
 beta2 = 0.999
-num_epochs = 1
+num_epochs = 5
 
 data_dir = "data/celeba"
 image_size = 64
@@ -34,6 +36,11 @@ real_label_identifier = 1
 fake_label_identifier = 0
 
 dataloader = data_utils.get_datloader(data_dir, image_size, batch_size, True, num_workers)
+G_losses = []
+D_losses = []
+gen_image_list = []
+total_iters = num_epochs * (len(dataloader) * batch_size)
+
 for epoch in range (num_epochs):
     for i, data in enumerate(dataloader, 0):
         # get rid of any residual gradients
@@ -48,20 +55,18 @@ for epoch in range (num_epochs):
         # create real_label and fake_label tensors to be used for calculating loss 
         real_label = torch.full((batch_size, 1), real_label_identifier, dtype=torch.float32)
         fake_label = torch.full((batch_size, 1), fake_label_identifier, dtype=torch.float32)
-        print (real_label.size())
-        print (fake_label.size())
-        
+
+
         ##################################
         ##    Training Discriminator    ##
-        # ################################ 
+        ################################## 
         
         # Train discriminator on real data
         output = netD(imgs).view(-1).unsqueeze(1)
-        print (output.size())
         errD_real = criterion(output, real_label)
         errD_real.backward()
 
-        # Train discriminator on fake data.
+        # Train discriminator on fake data
         # Generate fake data first
         noise = torch.randn(batch_size, latent_vector_size, 1, 1, dtype=torch.float32)
         fake_imgs = netG(noise)
@@ -79,10 +84,12 @@ for epoch in range (num_epochs):
 
         ##################################
         ##      Training Generator      ##
-        # ################################ 
+        ################################## 
+        
         # as we have applied optimizer.step on discriminator once,
-        #  we need to generate the output from discriminator once again.
+        # we need to generate the output from discriminator once again.
         output = netD(fake_imgs).view(-1).unsqueeze(1)
+        
         # While training the generator, real_label is the target
         errG = criterion(output, real_label)
         lossG = errG.item()
@@ -91,5 +98,38 @@ for epoch in range (num_epochs):
         # step through the optimizer for generator
         optimizerG.step()
         
-        
+        if (i % 50 == 0):
+            G_losses.append(errG.item())
+            D_losses.append(errD.item())
+
+            print ("Epoch {}/{}, Iteration: {}/{} \tLoss_G: {lg:.4f} \tLoss_D: {ld:.4f}".
+                    format(epoch+1, num_epochs, i+1, len(dataloader), lg=errG.item(), ld=errD.item()))
+
+        if (i % 500 == 0):
+            gen_image = netG(fixed_noise)
+            gen_image_list.append(gen_image)
         print ("\n")
+
+    netD_checkpoint = "checkpoint_netD_" + str(epoch) + ".pt"
+    netG_checkpoint = "checkpoint_netG_" + str(epoch) + ".pt"
+    torch.save(netD.state_dict(), netD_checkpoint)
+    torch.save(netG.state_dict(), netG_checkpoint)
+
+
+torch.save(netD.state_dict(), "netD_final.pt")
+torch.save(netG.state_dict(), "netG_final.pt")
+
+# plot the losses over iterations
+iter_step = (int) (total_iters/len(G_losses))
+iters = np.array(range(0, total_iters, iter_step))
+
+f = plt.figure()
+f.set_figwidth(8)
+f.set_figheight(8)
+plt.plot(iters, G_losses, color="red", label="G_loss")
+plt.plot(iters, D_losses, color="yellow", label="D_loss")
+plt.title("Losses vs Iterations")
+plt.ylabel("Loss")
+plt.xlabel("Iteration")
+plt.legend()
+plt.savefig("loss_iter.png")
