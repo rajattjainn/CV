63dbc4706f369feab740113816198b2e25ded047
Rajat Jain <tojainrajat@gmail.com>
2022-07-07 20:16:30 +0530

    [WIP]: Define one source of truth for object classes

--- CV/Pointnet/data_utils/shapenet.py
+++ CV/Pointnet/data_utils/shapenet.py
@@ -14,9 +14,15 @@ class Shapenet(Dataset):
         map_file = os.path.join(root_dir, "synsetoffset2category.txt")
         with open(map_file) as mf:
             lines = mf.readlines()
-            self.cls_fldr_map_dict = {i.split()[1]: i.split()[0] for i in lines}
-        folders = list(self.cls_fldr_map_dict.keys())
-        self.classes = {folders[i]:i for i in range((len(folders)))}
+            self.obj_fldr_map_dict = {i.split()[1]: i.split()[0] for i in lines}
+        # print (self.obj_fldr_map_dict)
+
+        cur_file_root_pth = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        clss_map_file = os.path.join(cur_file_root_pth, "assets", "class_mapping.txt")
+        with open(clss_map_file) as cmf:
+            lines = cmf.readlines()
+            self.classes = {i.split()[1]: i.split()[0] for i in lines}
+            # print (self.classes)
 
         split_file = json.load(open(os.path.join(root_dir, 
                     "train_test_split", "shuffled_{}_file_list.json".format(split_file))))
@@ -32,8 +38,9 @@ class Shapenet(Dataset):
         pt_array = np.array(rows).astype("float32")
         pt_tensor = torch.tensor(pt_array)
 
-        pt_cls = self.classes[cur_data[0]]
-        
+        pt_folder = cur_data[0]
+        pt_type = self.obj_fldr_map_dict[pt_folder]
+        pt_cls = self.classes[pt_type]
         return pt_tensor, pt_cls
 
     def __len__(self):
@@ -42,10 +49,10 @@ class Shapenet(Dataset):
 
 if __name__ == "__main__":
     root_dir = "/Users/eureka/Desktop/3D_Data/shapenetcore_partanno_segmentation_benchmark_v0"
-    shapenet = Shapenet(root_dir, "test")
+    shapenet = Shapenet(root_dir, "train")
 
-    # loader = DataLoader(shapenet, batch_size = 1)
-    # for index, (pt_tensor, pt_cls) in enumerate(loader):
-    #     if index == 0:
-    #         raise Exception ("Uh oh")
+    loader = DataLoader(shapenet, batch_size = 1)
+    for index, (pt_tensor, pt_cls) in enumerate(loader):
+        if index == 0:
+            raise Exception ("Uh oh")
 
